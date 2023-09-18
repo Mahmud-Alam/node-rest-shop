08cc463a39f9a2c91f198d3d04f3f7157c1bf3f6
mahmud-alam <official.mahmud23@gmail.com>
2023-09-18 09:45:41 +0600

    [CREATED] order routes with uid, and applied get(200 ok), post(201 created), and delete(200 ok) methods

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const productRoutes = require('./api/routes/Products');
+const orderRoutes = require('./api/routes/Orders');
 
 const app = express();
 
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
