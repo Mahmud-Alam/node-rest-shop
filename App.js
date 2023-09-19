f1a254acf6b784b03dfb4ce8b71564a0373bde5c
mahmud-alam <official.mahmud23@gmail.com>
2023-09-19 17:51:12 +0600

    parse our body of incomming request

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/Products');
 const orderRoutes = require('./api/routes/Orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+// Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
