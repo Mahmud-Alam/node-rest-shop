4c24030cd5b6786ef49bb7f4541fb2c778c7885a
mahmud-alam <official.mahmud23@gmail.com>
2023-09-20 19:40:47 +0600

    [CORS ERROR] Handling

--- App.js
+++ App.js
@@ -10,6 +10,22 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// CORS Error Handling
+app.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Origin','https://mahmudalam.com');      //if I want to restrict only for couples of domains
+    res.header('Access-Control-Allow-Origin','*');                              //If I allow for all domains, then use '*'
+    //res.header('Access-Control-Allow-Headers','*');                              //If I allow for all types of headers
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    
+    // Block the incoming request: if we are not returning immediately OPTIONS request's response, use next() method
+    next();
+});
+
 // Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
